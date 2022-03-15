461e3edc32cc66b03e1d384676f1d4a0395c6b3c
guru1205 <79328955+guru1205@users.noreply.github.com>
2022-03-15 16:16:33 +0530

    Update s3main.tf

--- s3main.tf
+++ s3main.tf
@@ -7,5 +7,5 @@ resource "aws_s3_bucket" "bkt" {
 resource "aws_s3_bucket_object" "object" {
   bucket = "guru-bucket15-2022"
   key    = "credentials"
-  source = "D:\\GuruDrive\\aws_credentials\\credentials"
+  source = "D:\\GuruDrive\\aws_credentials\\credentials.txt"
 }
